6db19546b8a27e7b60c7adf513b066bfbf2b9aae
zhhigh <zhhigh@163.com>
2017-07-29 22:04:16 +0800

    V0.002

--- src/listener/friend.ts
+++ src/listener/friend.ts
@@ -1,28 +1,15 @@
 
-//import { Logger, LoggerConfig } from "log4ts";
-import {greetingsMsg} from "./config";
-import {Contact} from "wechaty";
-//import {HsyUtil} from "../hsy-util";
-//import {HsyBotLogger} from "../datastore";
 
-//const logger = Logger.getLogger(`friend`);
+//import {greetingsMsg} from "./config";
+import {Contact} from "wechaty";
+
 
 exports = module.exports = async function onFriend(contact:Contact, request) {
+  console.log('auto accepted for ' + contact + ' with message: ');
   if (request) {  // 1. request to be friend from new contact
-    //await HsyBotLogger.logFriendRequest(request);
-    await request.accept();
-    await contact.say(greetingsMsg);
-    /*let contactList = await Contact.findAll();
-     if ((contactList.length > 4000 && contactList.length % 5 == 0)
-     || (contactList.length > 4900 && contactList.length % 2 == 0)
-     || (contactList.length > 4950)) {
-     let bigTeamRoom = await HsyUtil.findHsyBigTeamRoom();
-     await bigTeamRoom.say(`报告~，刚添加新朋友${contact.name()}，好室友小助手的好友数量已达到${contactList.length}`);
-     } else {
-     console.log(`Added a new friend, current friend number ${contactList.length}.`);
-     }
-     } else {        // 2. confirm friend ship
-     await console.log('new friend ship confirmed with ' + contact);
-     }*/
+    request.accept();
+    contact.say('hello!');
+    console.log('auto accepted for ' + contact + ' with message: ' + request.hello);
+
   };
 };
