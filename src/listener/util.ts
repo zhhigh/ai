d959e9deeac209300cf4d74ce6cc5cb9e4e2ed98
zhhigh <zhhigh@163.com>
2017-07-30 17:56:45 +0800

    V0.002

--- src/listener/util.ts
+++ src/listener/util.ts
@@ -1,8 +1,8 @@
 
 export class Util{
 
-    public async getRandomFromArray(): Promise<number> {
-        let randomList:number[] = [6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000,17000,18000,19000];
+    public  getRandomFromArray(): number {
+        let randomList:number[] = [6000,7000,8000,9000,10000,11000,12000,13000,14000,15000];
         let length              = randomList.length;
         let random      = Math.random() *( length - 1 ) + 1;
         let temp        = Math.round(random);
